acb4318459e1f451e9c6d6366acd46e833dc6e14
Alexey Borzenkov <snaury@gmail.com>
2009-12-26 01:43:58 +0300

    Add some tests for trees

--- ztests.c
+++ ztests.c
@@ -19,7 +19,8 @@
 
 #include <windows.h>
 
-#define TEST_PATHNAMES
+//#define TEST_PATHNAMES
+#define TEST_TREES
 
 #ifdef TEST_PATHNAMES
 
@@ -40,6 +41,80 @@ int main(int argc, char** argv)
   char test_pathname[MAX_PATH] = { 0 };
   GetFullPathName(TEST_PATHNAME, MAX_PATH, test_pathname, NULL);
   printf("Result: '%s'\n", test_pathname);
+  return 0;
+}
+
+#endif
+
+#ifdef TEST_TREES
+#include "vpak_tree.h"
+#include <stdio.h>
+#include <time.h>
+
+struct vpak_tree* alloc_int_node(int num)
+{
+  struct vpak_tree* node = vpak_tree_alloc(sizeof(int));
+  *VPAK_TREE_CAST(int*, node) = num;
+  return node;
+}
+
+int compare_int_node(const void* a, const void* b)
+{
+  int anum = *(const int*)a;
+  int bnum = *(const int*)b;
+  return anum - bnum;
+}
+
+int search_int_node(const void* a, const void* b)
+{
+  int anum = *(const int*)a;
+  int bnum = (int)b;
+  return anum - bnum;
+}
+
+void dump_tree_node(struct vpak_tree* node, int prefix)
+{
+  unsigned i;
+  for (i = 0; i < prefix*2; ++i) putchar(' ');
+  printf("n: %d\n", *VPAK_TREE_CAST(int*,node));
+  for (i = 0; i < prefix*2; ++i) putchar(' ');
+  printf("h: %d\n", node->height);
+  for (i = 0; i < prefix*2; ++i) putchar(' ');
+  printf("b: %d\n", node->balance);
+  if (node->l) {
+    for (i = 0; i < prefix*2; ++i) putchar(' ');
+    printf("l:\n");
+    dump_tree_node(node->l, prefix + 1);
+  }
+  if (node->r) {
+    for (i = 0; i < prefix*2; ++i) putchar(' ');
+    printf("r:\n");
+    dump_tree_node(node->r, prefix + 1);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  unsigned i, k;
+  struct vpak_tree* root = NULL;
+  const struct vpak_tree* node;
+  srand(time(NULL));
+  for (i = 0; i < 15; ++i) {
+    int n = rand() % 10;
+    printf("insert %d = %p\n", n, vpak_tree_insert(&root, alloc_int_node(n), compare_int_node));
+    if (i == 14)
+      k = n;
+  }
+  dump_tree_node(root, 0);
+  node = vpak_tree_find(root, (const void*)k, search_int_node);
+  if (!node) {
+    printf("node %d not found!\n", k);
+    return 1;
+  }
+  if (*VPAK_TREE_CAST(const int*,node) != k) {
+    printf("node mismatch %d != %d\n", *VPAK_TREE_CAST(const int*,node), k);
+    return 1;
+  }
   return 0;
 }
 
